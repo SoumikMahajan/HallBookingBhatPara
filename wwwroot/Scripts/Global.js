fcd5a9d776522ccb8f912f0e0c727bbe9ce6911b
SoumikMahajan <soumik800@gmail.com>
2025-07-16 23:03:33 +0530

    Changes

---
+++ wwwroot/Scripts/Global.js
@@ -0,0 +1,8 @@
+﻿const _BaseURL = window.location.origin;
+const currentlocation = window.location.href;
+const _QueryParameter = window.location.pathname.split('/');
+const actionName = _QueryParameter[2] || '';
+
+$(function () {
+    
+});
